5e8c89fd24a7bce1b76202226ee7a56c700eb4fb
LeoFer-co <lyon241296@gmail.com>
2025-03-05 22:17:12 -0500

    cambio de encode de guardado en mqtt subscriber.py: de windows a UTF-8 otra vez

--- backend/mqtt_subscriber.py
+++ backend/mqtt_subscriber.py
@@ -5,7 +5,7 @@ def on_connect(client, userdata, flags, rc):
     print("Conectado con resultado: " + str(rc))
     # Nos suscribimos al tópico que publica el ESP32
     client.subscribe("lab/equipo1/temperatura")
-
+    
 # Función callback que se ejecuta cuando se recibe un mensaje
 def on_message(client, userdata, msg):
     print(f"Mensaje recibido en {msg.topic}: {msg.payload.decode()}")
@@ -18,7 +18,9 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 print("Conectando al broker MQTT...")
+
 client.connect(broker, port, 60)
 
 # Permanece en un bucle infinito para procesar mensajes
+ 
 client.loop_forever()
