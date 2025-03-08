9a99f557b9a4e909584650dbc5137780d1c5e844
LeoFer-co <lyon241296@gmail.com>
2025-03-08 16:46:11 -0500

    modo debug False en app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -365,4 +365,4 @@ def save_order():
     return jsonify({"status": "Order saved successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
